8c156cac202454b9fd8e8ba1da5449edd0d41f90
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-11-29 17:48:58 +0600

    feature-selection: use chi square method find correlation between target and feature

--- gradient_boost_classifier/gbc_customer_sentiment.py
+++ gradient_boost_classifier/gbc_customer_sentiment.py
@@ -10,6 +10,7 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import chi2
 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -29,23 +30,53 @@ data = pd.read_csv("datasets/customer_sentiment.csv")
 # plt.savefig("gender_sentiment.png")
 
 # sentiment distribution by age group
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.countplot(data=data, x="age_group", hue="sentiment", ax=ax)
-ax.set_title(
-    "Sentiment Distribution by Age Group", fontsize=16, fontweight="bold", pad=20
-)
-ax.set_xlabel("Age Group", fontsize=12)
-ax.set_ylabel("Sentiment", fontsize=12)
+# fig, ax = plt.subplots(figsize=(10, 6))
+# sns.countplot(data=data, x="age_group", hue="sentiment", ax=ax)
+# ax.set_title(
+#     "Sentiment Distribution by Age Group", fontsize=16, fontweight="bold", pad=20
+# )
+# ax.set_xlabel("Age Group", fontsize=12)
+# ax.set_ylabel("Sentiment", fontsize=12)
 
 """
 From the countplot we see that sentimant is distributed more or less equally amonth all the genders.
-So we can say that the feature gender has no influence on sentiment. But before we conclude let's
+So we can say that the feature gender has no influence on sentiment. But before we conclude we will
 do the Chi-Square test to confirm. 
 """
+feature_encoders = {}
 
-plt.tight_layout()
-plt.show()
-plt.close()
+for column in data.columns:
+    encoder = LabelEncoder()
+    data[column] = encoder.fit_transform(data[column])
+    feature_encoders[column] = encoder
+
+# dropping review_text because label encoding text data as integers is statistically meaningless
+# for text based feature we need to use methods like TF-IDF, bag of words, embeddings (BERT)
+X = data.drop(["customer_id", "sentiment", "review_text"], axis=1)
+Y = data["sentiment"]
+
+feature_column_list = X.columns.tolist()
+
+chi_scores, p_values = chi2(X, Y)
+
+feature_chi2_result = pd.DataFrame(
+    {
+        "features": feature_column_list,
+        "chi_square_score": chi_scores,
+        "p_values": p_values,
+    }
+)
+
+feature_chi2_result = feature_chi2_result.sort_values(
+    by="chi_square_score", ascending=False
+)
+
+print(feature_chi2_result)
+feature_chi2_result.to_csv("datasets/feature_chi2_result.csv")
+
+# plt.tight_layout()
+# plt.show()
+# plt.close()
 
 
 # features = data.drop(["customer_id", "sentiment", "gender", "age_group"], axis=1)
